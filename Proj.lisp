abfa4bb094729a09e7d5bca850c58841ce6cfc27
Sérgio Miguel <a_k_a_bouncer@hotmail.com>
2013-05-02 18:10:32 +0100

    ve-frente | ve-abaixo já juntam blocos

--- Proj.lisp
+++ Proj.lisp
@@ -45,14 +45,6 @@
   h-blocos                    ; Hash com os blocos existentes		
 )
 
-; Função que junta 2 blocos da mesma cor
-; ---------------------------------------;
-; ARG1 - tabuleiro com as peças
-; ARG2 - hashtable dos blocos
-; ARG3 - chave do bloco a manter
-; ARG4 - chave do bloco que desaparece
-
-
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;	DEBUG
@@ -85,26 +77,33 @@
 ;;	FUNÇÔES AUXILIARES
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defun junta-blocos (tabuleiro *ht* chave-b1 chave-b2)
-  (let* ((b-aux (bloco-cor (gethash chave-b1 *ht*)))                 ; Referência para o bloco que se vai manter
+; Função que junta 2 blocos da mesma cor
+; ---------------------------------------;
+; ARG1 - tabuleiro com as peças
+; ARG2 - hashtable dos blocos
+; ARG3 - chave do bloco a manter
+; ARG4 - chave do bloco que desaparece
+
+(defun junta-blocos (tabuleiro ht chave-b1 chave-b2)
+  (let* ((b-aux (bloco-cor (gethash chave-b1 ht)))                   ; Referência para o bloco que se vai manter
          (l-aux (bloco-lista-pecas b-aux))                           ; Lista das peças do bloco que se vai manter
          (posx)
          (posy))
-  (loop for p-aux in (bloco-lista-pecas (gethash chave-b2 *ht*)) do
+    (print "entrou: junta-blocos")
+  (loop for p-aux in (bloco-lista-pecas (gethash chave-b2 ht)) do
         (setq posx (car (peca-pos p-aux)))                           ; Guarda a coordenada x da peça a ser mudada
         (setq posy (cdr (peca-pos p-aux)))                           ; Guarda a coordenada y da peça a ser mudada
         (setf (peca-bloco p-aux) chave-b1)                           ; Muda o bloco da peça
         (setq l-aux (append l-aux (list p-aux)))                     ; Insere a peça na lista do bloco original
         (setf (nth posx (nth posy tabuleiro)) p-aux))                ; Re-insere a peça no tabuleiro
   (setf (bloco-lista-pecas b-aux) l-aux)                             ; Coloca a nova lista no bloco original
-  (setf (gethash chave-b1 *ht*) b-aux)                               ; Atualiza o bloco original na HT
-  (remhash chave-b2 *ht*)))                                          ; Remove o 2º bloco da HT
+  (setf (gethash chave-b1 ht) b-aux)                                 ; Atualiza o bloco original na HT
+  (remhash chave-b2 ht)))                                            ; Remove o 2º bloco da HT
 
 ;; Por testar - Dificil pois após o estado inicial não há blocos da mesma cor e de blocos diferentes ao lado uns dos outros, apenas acontece após haver remoções de blocos
 
 (defun lista-blocos (tabuleiro x-ini x-fin y-ini y-fin n-lin n-col hash)
   (let* ((resul hash)
-         (contador 0)
          (p-aux (make-peca))
          (p-aux-dir (make-peca))
          (p-aux-baixo (make-peca)))
@@ -117,15 +116,14 @@
                       (setq p-aux-dir (nth (+ 1 posx) (nth posy tabuleiro)))
                       (if (= (peca-cor p-aux) (peca-cor p-aux-dir)) ; Se peça à direita for da mesma cor
                           (if (not (= (peca-bloco p-aux) (peca-bloco p-aux-dir))) ; Se forem da mesma cor mas blocos diferentes -> Merge blocos
-                              (junta-blocos (tabuleiro hash (peca-bloco p-aux) (peca-bloco p-aux-dir)))))))
+                              (junta-blocos tabuleiro hash (peca-bloco p-aux) (peca-bloco p-aux-dir))))))
                 
                 (if (not (>= posy (- n-lin 1))) ;; Vê Baixo
                     (progn
                       (setq p-aux-baixo (nth posx (nth (+ 1 posy) tabuleiro))) 
                       (if (= (peca-cor p-aux) (peca-cor p-aux-baixo)) ; Se peça em baixo for da mesma cor
                           (if (not (= (peca-bloco p-aux) (peca-bloco p-aux-baixo))) ; Se forem da mesma cor mas blocos diferentes -> Merge blocos
-                              (junta-blocos (tabuleiro hash (peca-bloco p-aux) (peca-bloco p-aux-baixo)))))))
-               ))
+                              (junta-blocos tabuleiro hash (peca-bloco p-aux) (peca-bloco p-aux-baixo))))))))               
     resul))
  
 
@@ -186,31 +184,39 @@
 
 (defun ve-frente (tabuleiro p-aux b-aux posx posy hash)
   (let* ((l-aux (bloco-lista-pecas b-aux))
-         (p-dir (nth (+ posx 1) (nth posy tabuleiro))))
+         (p-dir (nth (+ posx 1) (nth posy tabuleiro)))
+         (chave-1 (peca-bloco p-aux))
+         (chave-2 (peca-bloco p-dir)))
     (print "entrou: ve-frente")
-    (if (= (peca-cor p-aux) (peca-cor p-dir))                               ; Se o da frente for igual
-        (progn
-         (format t "Right Match On: posx: ~D posy: ~D ~% " posx posy)
-         (setf (peca-bloco p-dir) (peca-bloco p-aux))                       ; Junta a informação do bloco à peça da direita
-         (setf (nth (+ posx 1) (nth posy tabuleiro)) p-dir)                 ; Coloca a peça atualizada no tabuleiro
-         (setq l-aux (append l-aux (list p-dir)))                           ; Adiciona a peça à lista para atualizar o bloco
-         (setf (bloco-lista-pecas (gethash (peca-bloco p-dir) hash)) l-aux) ; Atualiza o bloco na hash
-))))
+    (if (= (peca-cor p-aux) (peca-cor p-dir))                                       ; Se o da frente for igual
+        (if (or (not chave-2) (= chave-1 chave-2))
+            (progn
+              (format t "Right Match On: posx: ~D posy: ~D ~% " posx posy)
+              (setf (peca-bloco p-dir) (peca-bloco p-aux))                            ; Junta a informação do bloco à peça da direita
+              (setf (nth (+ posx 1) (nth posy tabuleiro)) p-dir)                      ; Coloca a peça atualizada no tabuleiro
+              (setq l-aux (append l-aux (list p-dir)))                                ; Adiciona a peça à lista para atualizar o bloco
+              (setf (bloco-lista-pecas (gethash (peca-bloco p-dir) hash)) l-aux))     ; Atualiza o bloco na hash
+          (junta-blocos tabuleiro hash chave-1 chave-2)))))
+
 
 ; Atribui ou junta blocos a partir da peça por baixo no tabuleiro
 ; TODO: mudar nome da função, adicionar efeito dominó quando encontra uma peça da mesma cor com outro bloco associado
 
 (defun ve-abaixo (tabuleiro p-aux b-aux posx posy hash)
   (let* ((l-aux (bloco-lista-pecas b-aux))
-         (p-baixo (nth posx (nth (+ posy 1) tabuleiro))))
+         (p-baixo (nth posx (nth (+ posy 1) tabuleiro)))
+         (chave-1 (peca-bloco p-aux))
+         (chave-2 (peca-bloco p-baixo)))
     (print "entrou: ve-abaixo")
-    (if (= (peca-cor p-aux) (peca-cor p-baixo))                                    ; Se o da frente for igual
-        (progn
-          (format t "Down Match On: posx: ~D posy: ~D ~% " posx posy)              
-          (setf (peca-bloco p-baixo) (peca-bloco p-aux))                           ; Junta a informação do bloco à peça da direita
-          (setf (nth posx (nth (+ posy 1) tabuleiro)) p-baixo)                     ; Coloca a peça atualizada no tabuleiro
-          (setq l-aux (append l-aux (list p-baixo)))                               ; Adiciona a peça à lista para atualizar o bloco
-          (setf (bloco-lista-pecas (gethash (peca-bloco p-baixo) hash)) l-aux))))) ; Atualiza o bloco na hash
+    (if (= (peca-cor p-aux) (peca-cor p-baixo))                                       ; Se o da frente for igual
+        (if (or (not chave-2) (= chave-1 chave-2))
+          (progn
+            (format t "Down Match On: posx: ~D posy: ~D ~% " posx posy)              
+            (setf (peca-bloco p-baixo) (peca-bloco p-aux))                            ; Junta a informação do bloco à peça da direita
+            (setf (nth posx (nth (+ posy 1) tabuleiro)) p-baixo)                      ; Coloca a peça atualizada no tabuleiro
+            (setq l-aux (append l-aux (list p-baixo)))                                ; Adiciona a peça à lista para atualizar o bloco
+            (setf (bloco-lista-pecas (gethash (peca-bloco p-baixo) hash)) l-aux))     ; Atualiza o bloco na hash
+          (junta-blocos tabuleiro hash chave-1 chave-2)))))
 
 
 
@@ -221,13 +227,14 @@
   (let* ((tab (cria-tabuleiro problema (list-length (first problema))))
          (h-blocos (lista-blocos-estado-inicial tab 0 (- (list-length (first problema)) 1) 0 (- (list-length problema) 1) (list-length problema) (list-length (first problema)) (make-hash-table)))
          (estado-inicial (make-no :n-pecas (* (list-length problema) (list-length (first problema))) :n-blocos (hash-table-count h-blocos) :tabuleiro tab :l-blocos h-blocos))
-         (meh (lista-blocos tab 0 (- (list-length (first problema)) 2) 0 (- (list-length problema) 2) (list-length problema) (list-length (first problema)) h-blocos))
         ; (gera-sucessores	#'sucessores)
         ; (heuristica1		#'heur-melhor-primeiro)
         ; (heuristica2		#'heur-melhor-primeiro-posicao-menor)
         ; (heuristica-opt	#'heur-menor-altura)
          resul solucao)
     (print (print-tabuleiro tab (- (list-length problema) 1) (- (list-length (first problema)) 1)))
+    (print-hash h-blocos)
+    (junta-blocos tab h-blocos 0 4)
     (print-hash h-blocos)
     estado-inicial)
 )
